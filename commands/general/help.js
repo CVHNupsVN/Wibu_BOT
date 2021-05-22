12dfc6fabc08bfa8127c6a7009a53f17ae57e95f
chuhanh-0704 <81749820+chuhanh-0704@users.noreply.github.com>
2021-05-22 17:52:29 +0700

    Update help.js

--- commands/general/help.js
+++ commands/general/help.js
@@ -15,7 +15,7 @@ module.exports = {
             .setColor(`RANDOM`)
             .addField(`Prefix: `, prefix)
             .addField(`Kiểm tra độ trễ của bot: `, `ping`)
-            .addField(`Kiểm tra thời gian trược tuyến: `, `uptime`)
+            .addField(`Kiểm tra thời gian trực tuyến: `, `uptime`)
             .addField(`Lệnh dành cho wibu: `, help_1)
         message.channel.send(embed);
     }
