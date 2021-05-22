5b79ede1da000079674e5af7239de5e9c0f207a5
chuhanh-0704 <81749820+chuhanh-0704@users.noreply.github.com>
2021-05-22 16:29:30 +0700

    Create quote.js

---
+++ commands/wibu/quote.js
@@ -0,0 +1,23 @@
+const { MessageEmbed } = require('discord.js');
+const fetch = require("node-fetch");
+module.exports = {
+    config: {
+        name: 'quote',
+        aliases: ["caunoi"]
+    },
+    run: async (bot, message, args, client) => {
+      fetch('https://animechan.vercel.app/api/random')
+       .then(response => response.json())
+       .then(quote => {
+           let embed = new MessageEmbed()
+           .setAuthor(`Hỗ trợ bởi: animechan.vercel.app`)
+           .setColor('RANDOM')
+           .addField('Anime: ', quote.anime)
+           .addField('Nhân vật: ', quote.character)
+           .addField('Câu nói: ', quote.quote)
+           //.setFooter('Yêu cầu bởi: ', `${message.author.tag}`)
+           .setTimestamp()
+           message.channel.send(embed)
+       });
+    }
+}
