994ac5e647952d0ab147fe20f5ab1bc7a358dca4
chuhanh-0704 <81749820+chuhanh-0704@users.noreply.github.com>
2021-05-23 14:29:07 +0700

    Update ero.js

--- commands/nsfw/ero.js
+++ commands/nsfw/ero.js
@@ -1,20 +1,24 @@
-const nekoslife = require('nekos.life');
 const { MessageEmbed } = require('discord.js');
+const nekoslife = require('nekos.life');
 const neko = new nekoslife();
 
 module.exports = {
-    config: {
-        name: 'ero',
-        aliases: ["ero"]
-    },
-    run: async (bot, message, args, client) => {
-      neko_img = await neko.sfw.ero();
-      let embed = new MessageEmbed()
+	config: {
+		name: 'ero',
+		aliases: ['ero']
+	},
+	run: async (bot, message, args, Discord, client) => {
+		if (message.channel.nsfw === true) {
+			link = await neko.nsfw.ero();
+			let embed = new MessageEmbed()
 				.setAuthor(`Cẩn thận với đôi mắt của bạn đấy 🙈: `)
 				.setColor('RANDOM')
-				.setImage(neko_img.url)
+				.setImage(link.url)
 				.setFooter(`Yêu cầu bởi: `+ message.author.tag)
 				.setTimestamp();
 			message.channel.send(embed);
-    }
-}
+		} else {
+			message.channel.send('Đây không phải kênh NSFW!');
+		}
+	}
+};
