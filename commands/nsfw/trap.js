ec2ce8da83a07ddc020c18481026e23da9fd3ae8
chuhanh-0704 <81749820+chuhanh-0704@users.noreply.github.com>
2021-05-23 14:31:51 +0700

    Create trap.js

---
+++ commands/nsfw/trap.js
@@ -0,0 +1,24 @@
+const { MessageEmbed } = require('discord.js');
+const nekoslife = require('nekos.life');
+const neko = new nekoslife();
+
+module.exports = {
+	config: {
+		name: 'hentai',
+		aliases: ['hent']
+	},
+	run: async (bot, message, args, Discord, client) => {
+		if (message.channel.nsfw === true) {
+			link = await neko.nsfw.trap();
+			let embed = new MessageEmbed()
+				.setAuthor(`Cẩn thận với đôi mắt của bạn đấy 🙈: `)
+				.setColor('RANDOM')
+				.setImage(link.url)
+				.setFooter(`Yêu cầu bởi: `+ message.author.tag)
+				.setTimestamp();
+			message.channel.send(embed);
+		} else {
+			message.channel.send('Đây không phải kênh NSFW!');
+		}
+	}
+};
