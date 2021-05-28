0894056ab9717df6b5151bdb9f71e6e5999e969d
chuhanh-0704 <81749820+chuhanh-0704@users.noreply.github.com>
2021-05-28 11:40:56 +0700

    Update help.js

--- commands/general/help.js
+++ commands/general/help.js
@@ -8,7 +8,7 @@ module.exports = {
     },
     run: async (bot, message, args, Discord, client) => {
         let prefix = config_prefix.prefix
-        let help_1 = '`foxgirl` `neko` `nekog` `hentai` `dance` `quote` `waifu` `wink`'
+        let help_1 = '`dance` `foxgirl` `neko` `nekog` `hentai`  `quote` `waifu` `wink` `boobs` `ero` `trap`'
         let activities_1 = '`youtube` `pokernight`'
         
         let embed = new MessageEmbed()
