f7cee2197931754f68940ae95cad6f7af64a31a7
chuhanh-0704 <81749820+chuhanh-0704@users.noreply.github.com>
2021-06-01 15:27:29 +0700

    Create anime.js

---
+++ commands/fun/anime.js
@@ -0,0 +1,39 @@
+const { MessageEmbed } = require('discord.js');
+const malScraper = require('mal-scraper');
+
+module.exports = {
+    config: {
+        name: 'anime',
+        aliases: []
+    },
+  run: async (bot, message, args, client) => {
+    const search = `${args}`;
+    if (!search)
+      return message.reply('Bạn quên cho tên tên anime vào à ?');
+      
+    malScraper.getInfoFromName(search)
+      .then((data) => {
+        const embed = new MessageEmbed()
+          .setAuthor(`Kết quả trả về từ My Anime List của ${args}`.split(',').join(' '))
+          .setThumbnail(data.picture)
+          .setColor('RANDOM')
+          .addField('Thể loại:', `\`${data.genres}\``, true)
+          .addField('Tiêu đề tiếng Anh:', `\`${data.englishTitle}\``, true)
+          .addField('Tiêu đề tiếng Nhật:', `\`${data.japaneseTitle}\``, true)
+          .addField('Thể loại:', `\`${data.type}\``, true)
+          .addField('Số tập:', `\`${data.episodes}\``, true)
+          .addField('Đánh giá:', `\`${data.rating}\``, true)
+          .addField('Đã phát sóng vào:', `\`${data.aired}\``, true)
+          .addField('Điểm số:', `\`${data.score}\``, true)
+          .addField('Rank:', `\`${data.ranked}\``, true)
+          .addField('Thời lượng:', `\`${data.duration}\``, true)
+          .addField('Studios:', `\`${data.studios}\``, true)
+          .addField('Độ phổ biến:', `\`${data.popularity}\``, true)
+          .addField('Trạng thái:', `\`${data.status}\``, true)
+          .setTimestamp()
+          .setFooter(`Yêu cầu bởi: ${message.author.tag}`,  message.author.displayAvatarURL({ dynamic: true }))
+        message.channel.send(embed);
+
+      })
+  }
+};
