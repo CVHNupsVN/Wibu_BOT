c13c7e1a1be6245924cb46453cb0d28c12dee02b
chuhanh-0704 <81749820+chuhanh-0704@users.noreply.github.com>
2021-06-01 15:20:32 +0700

    Create hug.js

---
+++ commands/fun/hug.js
@@ -0,0 +1,27 @@
+const discord = require("discord.js");
+const client = require('nekos.life');
+const neko = new client();
+
+module.exports = {
+    config: {
+        name: 'hug',
+        aliases: ['ôm']
+    },
+
+  run: async (client, message, args) => {
+    
+    let target = message.mentions.members.first()
+
+    if (!target) return message.reply(`Bạn cần phải chọn người để hôn !`)
+
+    let hug_img = await neko.sfw.hug();
+    
+    let embed = new discord.MessageEmbed()
+    .setImage(hug_img.url)
+    .setColor("RANDOM")
+    .setAuthor(`${message.author.username} đã ôm ${target.user.username}`)
+    .setTimestamp()
+    
+    message.channel.send(embed);
+  }
+};
