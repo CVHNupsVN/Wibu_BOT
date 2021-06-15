fc5df081053e4ad51fd24ab965a7997dafbcd072
chuhanh-0704 <81749820+chuhanh-0704@users.noreply.github.com>
2021-06-15 20:39:06 +0700

    Update message.js

--- events/guild/message.js
+++ events/guild/message.js
@@ -3,9 +3,11 @@ const { fs } = require('fs');
 const { prefix } = require('../../config.js');
 
 module.exports = async (bot, message) => {
-    if(message.author.bot || message.channel.type === "dm" || !message.content.startsWith(prefix)) return;
+  if (message.author.bot || message.channel.type === "dm" || !message.content.toLowerCase().startsWith(prefix)) return;
+  if (message.content.startsWith(prefix) || message.content.startsWith(prefix.toLowerCase()) || message.content.startsWith(prefix.toUpperCase())) {
     const args = message.content.slice(prefix.length).trim().split(/ +/g);
     const cmd = args.shift().toLowerCase();
     let commandfile = bot.commands.get(cmd) || bot.commands.get(bot.aliases.get(cmd));
-    if(commandfile) commandfile.run(bot, message, args);
+    if (commandfile) commandfile.run(bot, message, args);
+  }
 }
