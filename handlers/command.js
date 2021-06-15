1422ef350f32aac381b6d2e8d16310819daf252e
chuhanh-0704 <81749820+chuhanh-0704@users.noreply.github.com>
2021-06-15 20:11:05 +0700

    Update command.js

--- handlers/command.js
+++ handlers/command.js
@@ -1,14 +1,13 @@
 const { readdirSync } = require(`fs`);
 
-module.exports = (bot) => {
-    const load = dirs => {
-        const commands = readdirSync(`./commands/${dirs}/`).filter(d => d.endsWith(`.js`));
+module.exports = (client) => {
+     readdirSync("./commands/").forEach(dir => {
+        const commands = readdirSync(`./commands/${dir}/`).filter(d => d.endsWith(`.js`));
         for(let file of commands) {
-            let pull = require(`../commands/${dirs}/${file}`);
-            bot.commands.set(pull.config.name, pull);
-            if(pull.config.aliases) pull.config.aliases.forEach(a => bot.aliases.set(a, pull.config.name));
+            let pull = require(`../commands/${dir}/${file}`);
+            client.commands.set(pull.config.name, pull);
+            if(pull.config.aliases) pull.config.aliases.forEach(a => client.aliases.set(a, pull.config.name));
             console.log(`Lệnh | ${pull.config.name} đã tải thành công`)
         }
-    };
-    ["general", "wibu", "activities", "nsfw", "mod", "fun"].forEach(x => load(x));
+    })
 }
